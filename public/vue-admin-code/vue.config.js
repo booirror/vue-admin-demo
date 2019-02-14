78eb8ed1138fd185e4f1a5529fe129beb0d8523c
hardphp <hardphp@163.com>
2019-02-14 13:19:35 +0800

    baseUrl 废弃，改成publicPath

--- public/vue-admin-code/vue.config.js
+++ public/vue-admin-code/vue.config.js
@@ -16,7 +16,7 @@ const BASE_URL = process.env.NODE_ENV === 'production'
   : '/'
 
 module.exports = {
-  baseUrl: BASE_URL,
+  publicPath: BASE_URL,
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
